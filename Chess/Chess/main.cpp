de2abb9118844d9a84de57c92beb8dadd60265e3
adlane98 <ladjal.adlane@gmail.com>
2018-06-28 18:11:01 +0200

    Add the XCode project to the repo

---
+++ Chess/Chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Adlane Ladjal on 28/06/2018.
+//  Copyright © 2018 Adlane Ladjal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
